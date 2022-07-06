6c7dd5b26df89318295488c6cb5ca7265e294ec8
Eric Arnhem <arnhemeric60@gmail.com>
2022-07-06 21:24:44 +0200

    Add function to find a Starship Class by id

--- app/controllers/starship_class.controller.js
+++ app/controllers/starship_class.controller.js
@@ -10,6 +10,24 @@ exports.create = (req, res) => {
 // Find a single Starship Class by id
 exports.findOneById = (req, res) => {
 
+  const id = req.params.id;
+
+  StarshipClass.findByPk(id)
+    .then(data => {
+      if (data) {
+        res.send(data);
+      } else {
+        res.status(404).send({
+          message: `Cannot find a Starship Class with id=${id}.`
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error retrieving a Starship Class with id=" + id
+      });
+    });
+
 };
 
 // Find a single Starship Class by name
