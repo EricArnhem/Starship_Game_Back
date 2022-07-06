24a2e6b46434845ca25e4e4f4a8bb6fa6f941350
Eric Arnhem <arnhemeric60@gmail.com>
2022-07-06 20:50:03 +0200

    Add CRUD functions declarations

---
+++ app/controllers/starship.controller.js
@@ -0,0 +1,38 @@
+const db = require("../models");
+const Starship = db.starship;
+const Op = db.Sequelize.Op;
+
+// Create a new Starship
+exports.create = (req, res) => {
+
+};
+
+// Find a single Starship by id
+exports.findOneById = (req, res) => {
+
+};
+
+// Find a single Starship by name
+exports.findOneByName = (req, res) => {
+
+};
+
+// Find all Starships
+exports.findAll = (req, res) => {
+
+};
+
+// Find all Starships of a class
+exports.findAllOfClass = (req, res) => {
+
+};
+
+// Update a single Starship by id
+exports.updateById = (req, res) => {
+
+};
+
+// Delete a single Starship by id
+exports.deleteById = (req, res) => {
+
+};
---
+++ app/controllers/starship_class.controller.js
@@ -0,0 +1,33 @@
+const db = require("../models");
+const StarshipClass = db.starshipClass;
+const Op = db.Sequelize.Op;
+
+// Create a new Starship Class
+exports.create = (req, res) => {
+
+};
+
+// Find a single Starship Class by id
+exports.findOneById = (req, res) => {
+
+};
+
+// Find a single Starship Class by name
+exports.findOneByName = (req, res) => {
+
+};
+
+// Find all Starships Classes
+exports.findAll = (req, res) => {
+
+};
+
+// Update a single Starship Class by id
+exports.updateById = (req, res) => {
+
+};
+
+// Delete a single Starship Class by id
+exports.deleteById = (req, res) => {
+
+};
