86f60fe94375ced3c2ccf38dd469855314c2c569
Eric Arnhem <arnhemeric60@gmail.com>
2022-07-19 21:19:21 +0200

    Update comments

--- app/routes/starship_class.routes.js
+++ app/routes/starship_class.routes.js
@@ -3,16 +3,16 @@ module.exports = app => {
   const starship_class = require("../controllers/starship_class.controller.js");
   var router = require("express").Router();
 
-  // Create a new Starship Class
+  // Create a new Starship class
   router.post("/", starship_class.create);
 
-  // Retrieve a single Starship class by id
+  // Find a single Starship class by id
   router.get("/:id", starship_class.findOneById);
 
-  // Retrieve a single Starship class by name
+  // Find a single Starship class by name
   router.get("/name/:name", starship_class.findOneByName);
 
-  // Retrieve the fuel capacity of a Starship class by id
+  // Find the fuel capacity of a Starship class by id
   router.get("/:id/fuel-capacity", starship_class.getFuelCapacityById);
 
   app.use('/api/starship-class', router);
